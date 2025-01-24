7c3ab73108ef3415f72cea9be326b5f75e53859a
kogebngadak <kogebngadak@gmail.com>
2025-01-24 21:58:15 +0000

    commited at 45712525

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,7 @@ module.exports = {
     "./src/**/*.{html,ts}",
     "./node_modules/flyonui/dist/js/*.js"
   ],
-  plugins: [
-    require("flyonui"),
-    require("flyonui/plugin")
-  ],
+  plugins: [],
   theme: {
     extend: {},
   },
